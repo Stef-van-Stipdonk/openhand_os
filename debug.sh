fc990a2ed7f43afccbd2f2cd6d38c3168f4496f1
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-02-21 16:36:51 +0100

    formatting changes

--- debug.sh
+++ debug.sh
@@ -2,4 +2,7 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -s -S -cdrom openhand.iso
+qemu-system-$(./target-triplet-to-arch.sh $HOST)\
+  -s \
+  -S \
+  -cdrom openhand.iso
