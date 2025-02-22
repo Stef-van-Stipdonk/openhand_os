e85b57152883ca22e735ad0e7b6be6ff05cd5a88
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-02-22 20:16:05 +0100

    build.sh now takes arguments: - Now takes command line arguments, if no arguments are provided, defaults to - Always add the  flag.

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 set -e
 . ./headers.sh
 
+DEF_ARGS=install
+
+if [ ! $# -eq 0 ]; then
+  DEF_ARGS=$@
+fi
+
 for PROJECT in $PROJECTS; do
-  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
+  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE $DEF_ARGS -j $(nproc))
 done
