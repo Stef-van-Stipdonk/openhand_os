b0953e69a08215c132542fe350c9be07daaf4945
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2025-03-13 20:00:58 +0100

    Populate IDT and setup ISR stubs"
    Populated the IDT with the required gates and setup an interrupt handler stub.

--- kernel/arch/i386/exception_handler.c
+++ kernel/arch/i386/exception_handler.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
 __attribute__((__noreturn__))
-	      void exception_handler();
+	      void exception_handler(uint32_t error_code);
 
-void exception_handler() {
+void exception_handler(uint32_t error_code) {
+	if (error_code) {
+		printf("test");
+	}
+
 	printf("Interrupt handled\n");
-
 	__asm__ volatile ("cli; hlt"); // hang
 	
 	__builtin_unreachable();
--- kernel/arch/i386/isr_error_stub.nasm
+++ kernel/arch/i386/isr_error_stub.nasm
@@ -4,13 +4,17 @@ global isr_stub_table
 
 %macro isr_err_stub 1
 isr_stub_%+%1:
+	push %1
 	call exception_handler
+	add esp, 4
 	iret
 %endmacro
 
 %macro isr_no_err_stub 1
 isr_stub_%+%1:
+	push %1
 	call exception_handler
+	add esp, 4
 	iret
 %endmacro
 
